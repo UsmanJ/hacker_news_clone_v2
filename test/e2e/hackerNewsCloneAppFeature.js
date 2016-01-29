8cc0006f74f75ade458e12fadb979472e320d75e
Usman Jamil <usman.jamil@live.co.uk>
2016-01-29 14:37:35 +0000

    added protractor to project

---
+++ test/e2e/hackerNewsCloneAppFeature.js
@@ -0,0 +1,19 @@
+describe('itunes search api application', function() {
+
+  var searchBox = element(by.css('.input'));
+  var searchBtn = element(by.css('#searchBtn'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:9000/#/');
+  })
+
+  it('has a title', function() {
+    expect(browser.getTitle()).toEqual('itunesSearchApiApp');
+  });
+
+  it('displays the results once the query is run', function() {
+    searchBox.sendKeys('Adele');
+    searchBtn.click();
+    expect(element(by.className('results')).isDisplayed()).toBeTruthy();
+  });
+});
